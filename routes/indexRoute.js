16b86bd1073df03b9fc201f0256d2ff8243a509e
unknown <muofunanyajennifer1@gmail.com>
2023-09-10 17:06:29 +0100

    updated the url

--- routes/indexRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getSlackName } = require('../controllers/indexController')
-const router = express.Router();
-
-router.get('/slackname', getSlackName)
-
-module.exports = router
