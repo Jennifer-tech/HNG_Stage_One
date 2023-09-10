ad5d771bd9efb62e568066acc40a938601b6ffaf
unknown <muofunanyajennifer1@gmail.com>
2023-09-10 21:12:43 +0100

    fixed error

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 
 exports.getSlackName = (req, res) => {
     try{
@@ -6,8 +7,9 @@ exports.getSlackName = (req, res) => {
         const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         const now = new Date();
         const current_day = daysOfWeek[now.getUTCDay()]
-        const utc_time = new Date().toISOString();
-        
+        // const utc_time = new Date().toISOString();
+        const utc_time = moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]');
+
         const jsonResponse = {
             slack_name,
             current_day,
